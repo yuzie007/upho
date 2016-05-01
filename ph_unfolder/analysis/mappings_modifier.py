f5c1b30874f63be2fa2ae43254484d533da3a3da
ikeda <ikeda@IKEDA-MBA.local>
2016-05-01 23:31:47 +0900

    Add mappings_modifier

---
+++ ph_unfolder/analysis/mappings_modifier.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+__author__ = "Yuji Ikeda"
+
+import numpy as np
+
+
+class MappingsModifier(object):
+    def __init__(self, mappings):
+        self.set_mappings(mappings)
+
+    def set_mappings(self, mappings):
+        self._mappings = np.array(mappings)
+
+    def get_mappings(self):
+        return self._mappings.copy()
+
+    def invert_mappings(self):
+        mappings = self._mappings
+        mappings_inverse = -1 * np.zeros_like(mappings)
+        for i, mapping in enumerate(mappings):
+            mappings_inverse[i] = self._invert_mapping(mapping)
+        return mappings_inverse
+
+    def _invert_mapping(self, mapping):
+        mapping_inverse = -1 * np.zeros_like(mapping)
+        for index, value in enumerate(mapping):
+            mapping_inverse[value] = index
+        return mapping_inverse
+
+    def expand_mappings(self, n, is_inverse=False):
+        if is_inverse:
+            mappings = self.invert_mappings()
+        else:
+            mappings = self._mappings
+
+        expanded_mappings = np.repeat(mappings, n, axis=1)
+        expanded_mappings *= n
+        for i in range(n):
+            expanded_mappings[:, i::n] += i
+        return expanded_mappings
---
+++ test/test_mappings_modifier.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division, absolute_import, print_function
+
+__author__ = "Yuji Ikeda"
+
+import unittest
+import numpy as np
+from ph_unfolder.analysis.mappings_modifier import MappingsModifier
+
+
+class TestMappingsInverter(unittest.TestCase):
+    def test_invert_mappings(self):
+        mappings = [
+            [0, 1, 2, 3],
+            [0, 2, 1, 3],
+            [1, 2, 3, 0],
+        ]
+        mappings_inv_expected = [
+            [0, 1, 2, 3],
+            [0, 2, 1, 3],
+            [3, 0, 1, 2],
+        ] 
+
+        mappings_inv = MappingsModifier(mappings).invert_mappings()
+        self.assertTrue(np.all(mappings_inv == mappings_inv_expected))
+
+    def test_expand_mappings(self):
+        mappings = [
+            [0, 1, 2, 3],
+            [0, 2, 1, 3],
+            [1, 2, 3, 0],
+        ]
+        n = 3
+        expanded_mappings_expected = [
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+            [0, 1, 2, 6, 7, 8, 3, 4, 5, 9, 10, 11],
+            [3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2],  # Good
+        ]
+
+        expanded_mappings = MappingsModifier(mappings).expand_mappings(n)
+        self.assertTrue(
+            np.all(expanded_mappings == expanded_mappings_expected))
+
+    def test_expand_mappings_inv(self):
+        mappings = [
+            [0, 1, 2, 3],
+            [0, 2, 1, 3],
+            [1, 2, 3, 0],
+        ]
+        n = 3
+        expanded_mappings_inv_expected = [
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+            [0, 1, 2, 6, 7, 8, 3, 4, 5, 9, 10, 11],
+            [9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8],
+        ]
+
+        mappings_modifier = MappingsModifier(mappings)
+        expanded_mappings_inv = mappings_modifier.expand_mappings(
+            n, is_inverse=True)
+        self.assertTrue(
+            np.all(expanded_mappings_inv == expanded_mappings_inv_expected))
+
+
+if __name__ == "__main__":
+    unittest.main()
