d554abbdc11f68ac6dca4e504d36266570026c3d
ikeda <ikeda@IKEDA-MBA.local>
2016-12-05 18:58:39 +0900

    Fix setup.py to work correctly

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python
 from distutils.core import setup
+
 packages = ['ph_unfolder',
             'ph_unfolder.phonon',
+            'ph_unfolder.harmonic',
             'ph_unfolder.analysis',
-            'ph_unfolder.structure']
-scripts = ['scripts/ph_unfolder']
-setup(name='ph_unfolder',
-      version='0.0.0',
+            'ph_unfolder.structure',
+            'ph_unfolder.irreps',
+            'group',
+]
+scripts = [
+    'scripts/upho_weights',
+    'scripts/upho_sf',
+]
+setup(name='upho',
+      version='0.4.0',
       author="Yuji Ikeda",
       author_email="ikeda.yuji.6m@kyoto-u.ac.jp",
       packages=packages,
