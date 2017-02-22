Fix to calculate average masses for each inequivalent site
Before symmetrically inequivalent sites for "unitcell_ideal" were not
distinguished; the averages are done only according to the number of atoms for
each dummy "element".
This might cause problems for systems with two or more sublattices such
as the omega structure and the the dhcp structure.
Now the averages are taken over each set of symmetrically equivalent sites.

ph_unfolder/api_unfolding.py
@@ -294,7 +294,7 @@ class PhonopyUnfolding(Phonopy):
 
     def average_masses(self):
 
-        calculate_average_masses(self._unitcell, self._unitcell_ideal)
+        calculate_average_masses(self._unitcell, Symmetry(self._unitcell_ideal))
 
         self._build_supercell()
         self._build_primitive_cell()
@@ -315,14 +315,22 @@ class PhonopyUnfolding(Phonopy):
         self.set_force_constants(fc_average)  # Dynamical matrices are also prepared inside.
 
 
-def calculate_average_masses(unitcell, unitcell_ideal):
+def calculate_average_masses(unitcell, symmetry):
+    """Calculate avearge masses from the ideal structure
+
+    Parameters
+    ----------
+    unitcell : Cell
+    symmetry : Symmetry obtained from unitcell_ideal
+    """
     # TODO(ikeda): Now atomic order is supposed to be the same, which should be modified.
-    symbols_ideal = unitcell_ideal.get_chemical_symbols()
-    reduced_symbols_ideal = sorted(set(symbols_ideal), key=symbols_ideal.index)
+    map_atoms = symmetry.get_map_atoms()
+    independent_atoms = symmetry.get_independent_atoms()
+
     masses = unitcell.get_masses()
     masses_average = np.zeros_like(masses)
-    for rs in reduced_symbols_ideal:
-        indices = np.char.strip((symbols_ideal)) == np.char.strip(rs)
+    for ia in independent_atoms:
+        indices = (map_atoms == ia)
         mass_average = np.average(masses[indices])
         masses_average[indices] = mass_average
     unitcell.set_masses(masses_average)

test/test_average_masses.py
@@ -5,6 +5,7 @@ from __future__ import (absolute_import, division,
 import unittest
 import numpy as np
 from phonopy.interface.vasp import read_vasp
+from phonopy.structure.symmetry import Symmetry
 from ph_unfolder.api_unfolding import calculate_average_masses
 
 
@@ -20,12 +21,13 @@ def create_msg(list1, list2):
 
 class TestAverageMasses(unittest.TestCase):
     def setUp(self):
-        self._unitcell_ideal = read_vasp('poscars/POSCAR_omega_ideal')
+        unitcell_ideal = read_vasp('poscars/POSCAR_omega_ideal')
+        self._symmetry = Symmetry(unitcell_ideal)
         self._prec = 1e-12
 
     def test_1(self):
         unitcell = read_vasp('poscars/POSCAR_omega_disordered_1')
-        calculate_average_masses(unitcell, self._unitcell_ideal)
+        calculate_average_masses(unitcell, self._symmetry)
 
         masses = unitcell.get_masses()
         masses_expected = np.array([47.867, 91.224, 91.224])
@@ -35,7 +37,7 @@ class TestAverageMasses(unittest.TestCase):
         
     def test_2(self):
         unitcell = read_vasp('poscars/POSCAR_omega_disordered_2')
-        calculate_average_masses(unitcell, self._unitcell_ideal)
+        calculate_average_masses(unitcell, self._symmetry)
 
         masses = unitcell.get_masses()
         masses_expected = np.array([47.867, 69.5455, 69.5455])