7a3ab033a3356bd1d68b2f55b239b36373b0d949
ikeda <ikeda@IKEDA-MBA.local>
2017-09-01 21:11:44 +0200

    Modify to use yaml instead of json

--- upho/file_io.py
+++ upho/file_io.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
-
-__author__ = "Yuji Ikeda"
-
 import numpy as np
+
+__author__ = 'Yuji Ikeda'
 
 
 def read_band_yaml(yaml_file="band.yaml"):
@@ -14,7 +13,7 @@ def read_band_yaml(yaml_file="band.yaml"):
     npath = data['npath']
     distance = np.zeros(nqpoint)
     frequency = []
-    weight    = []
+    weight = []
     for iqpoint in range(nqpoint):
         distance[iqpoint] = data['phonon'][iqpoint]['distance']
         nband = len(data['phonon'][iqpoint]['band'])
@@ -24,7 +23,7 @@ def read_band_yaml(yaml_file="band.yaml"):
             f[i] = data['phonon'][iqpoint]['band'][i]['frequency']
             w[i] = data['phonon'][iqpoint]['band'][i]['weight']
         frequency.append(f)
-        weight   .append(w)
+        weight.append(w)
 
     nsep = nqpoint // npath
 
@@ -41,7 +40,7 @@ def read_band_hdf5(hdf5_file="band.hdf5"):
 
 
 def read_input(filename_input):
-    import json
-    with open(filename_input, "r") as f:
-        dict_input = json.load(f)
+    import yaml
+    with open(filename_input, 'r') as f:
+        dict_input = yaml.load(f)
     return dict_input
