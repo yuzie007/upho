01a6a1b4f6346aab6a8050012bf7c44d87483ac7
ikeda <ikeda@IKEDA-MBA.local>
2018-02-22 14:37:32 +0100

    Update the version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ scripts = [
     'scripts/upho_sf',
 ]
 setup(name='upho',
-      version='0.5.1',
+      version='0.5.3',
       author="Yuji Ikeda",
       author_email="ikeda.yuji.6m@kyoto-u.ac.jp",
       packages=packages,
