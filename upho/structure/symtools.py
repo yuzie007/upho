599507e1332f578c4ba7e176e0896b37d5a687b6
sheikh ahnaf <ahnafakifalvi2@gmail.com>
2020-09-08 23:43:15 +0600

    Update symtools.py
    the line "from phonopy.structure import spglib" throws an error. I checked phonopy.structure and couldn't find spglib there.maybe importing spglib package directly will solve the problem.

--- upho/structure/symtools.py
+++ upho/structure/symtools.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 import numpy as np
-from phonopy.structure import spglib
+import spglib
 
 
 def get_rotations_cart(atoms):
